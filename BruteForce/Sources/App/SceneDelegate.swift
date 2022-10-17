88b09fed7d7ea4960f5be72843b179d864c7e1b1
Arthur <arturgorlov5@gmail.com>
2022-10-17 22:16:21 +0300

    First project settings.

---
+++ BruteForce/Sources/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  BruteForce
+//
+//  Created by Arthur on 17.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let PasswordViewController = PasswordViewController()
+        window?.rootViewController = PasswordViewController
+        window?.makeKeyAndVisible()
+    }
+}
--- BruteForce/Sources/Controllers/PasswordViewController.swift
+++ BruteForce/Sources/Controllers/PasswordViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PasswordViewController.swift
 //  BruteForce
 //
 //  Created by Arthur on 17.10.2022.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PasswordViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
